bf1c8af967e5f58416de1285867118fbd3d15a67
duyang <79229788@qq.com>
2017-08-23 15:31:07 +0800

    update

--- database/modules/init.js
+++ database/modules/init.js
@@ -2,18 +2,19 @@ const CB = require('./cb');
 const _ = require('lodash');
 const PG = require('pg');
 const OSS = require('ali-oss');
+const Redis = require('redis');
 
 /**
- * 初始化数据库
+ * 初始化默认数据库
  * @param config
  */
 CB.initPG = function (config) {
   CB.pgConfig = _.extend({
     host     : '',
-    port     : 0,
+    port     : 8888,
     user     : '',
     password : '',
-    database : '',
+    database : 'web',
     printSql : false,
   }, config || {});
   CB.pg = new PG.Pool(CB.pgConfig);
@@ -24,7 +25,7 @@ CB.initPG = function (config) {
 };
 
 /**
- * 初始化对象存储
+ * 初始化默认OSS
  * @param config
  */
 CB.initOSS = function (config) {
@@ -32,9 +33,35 @@ CB.initOSS = function (config) {
     region          : '',
     accessKeyId     : '',
     accessKeySecret : '',
-    bucket          : '',
+    bucket          : 'web-user-norm',
   }, config || {});
+
   CB.oss = new OSS(CB.ossConfig);
 };
+/**
+ * 初始化网站静态资源OSS
+ * @param config
+ */
+CB.initStaticOSS = function (config) {
+  CB.staticOSSConfig = _.extend({
+    region          : '',
+    accessKeyId     : '',
+    accessKeySecret : '',
+    bucket          : 'web-static-resource',
+  }, config || {});
+  CB.staticOSS = new OSS(CB.staticOSSConfig);
+};
 
+/**
+ * 初始化Session Redis
+ * @param config
+ */
+CB.initSessionRedis = function (config) {
+  CB.sessionRedisConfig = _.extend({
+    host: '',
+    port: 8888,
+    password : '',
+  }, config || {});
+  CB.sessionRedis = Redis.createClient(sessionRedisConfig);
+};
 
